07ebaf2d184c7548987a8c0512f352ec532f344e
midorigoke <midorigoke@desumachi.tk>
2017-11-25 19:04:54 +0900

    update: エラーはstderrへ

--- notify.sh
+++ notify.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$BASH_SOURCE")"
 
 jq '.accounts[] as $account | (select($account.IPv4) | $account.id, $account.password, .endpoints.IPv4, "IPv4"), (select($account.IPv6) | $account.id, $account.password, .endpoints.IPv6, "IPv6")' settings.json |
 xargs -L4 |
-awk '{print "curl -sS -u "$1":"$2" "$3" | grep -F \"Login and IP address notify OK.\" >/dev/null || echo Notification Failed id:"$1" protocol:"$4}' |
+awk '{print "curl -sS -u "$1":"$2" "$3" | grep -F \"Login and IP address notify OK.\" >/dev/null || echo Notification Failed id:"$1" protocol:"$4" >&2"}' |
 sh
